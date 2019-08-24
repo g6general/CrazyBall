f608403dfe6c841f12e71fcf678bac13c4bc509b
gontarevds <gontarevds@gmail.com>
2019-08-24 13:49:16 +0300

    progressBar-done

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    public RectTransform progressBar;
+    public bool ballMoving = true;
+    public int levelLength;
+    public float blockLength;
+    public float speed;
+
+    private float progressBarMovementSpeed;
+
+    private void Start()
+    {
+        progressBarMovementSpeed = speed / (levelLength * blockLength);
+    }
+
+    void Update()
+    {
+        
+        if (ballMoving)
+        {
+            progressBar.sizeDelta = new Vector2(progressBar.sizeDelta.x+progressBarMovementSpeed*Time.deltaTime, 100);
+        }
+    }
+}
