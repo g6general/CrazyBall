a2e78242f2af1ff1d9640958f992f62befe6726a
Alexey <kalinichenkoae@gmail.com>
2019-08-30 10:05:29 +0300

    add progress bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    private RectTransform mProgressBar;
+    private float mSphereSpeedCoef;
+
+    void Start()
+    {
+        mProgressBar = GameObject.Find("ProgressBarFilled").GetComponent<RectTransform>();
+        
+        var parameters = GameObject.Find("MainObject").GetComponent<Parameters>();
+        var levelLength = parameters.getLength() * parameters.mBlockSizeZ;
+        var sphereSpeed = parameters.mHorizontalSpeed;
+        
+        mSphereSpeedCoef = sphereSpeed / levelLength;
+    }
+
+    void Update()
+    {
+        var horizontalStep = mSphereSpeedCoef * Time.deltaTime;
+        mProgressBar.localScale = new Vector3(mProgressBar.localScale.x + horizontalStep, mProgressBar.localScale.y);
+    }
+}
